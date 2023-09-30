572a955e7de8be70398bc0d8c954da5e5217cd2e
prashantkale <prashantkale15@gmail.com>
2023-09-30 21:08:47 +0530

    extent code added

--- src/test/java/test/FacebookLoginTest.java
+++ src/test/java/test/FacebookLoginTest.java
@@ -3,12 +3,16 @@ package test;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 
 import pojo.LaunchBrowser;
 import pom.FacebookLoginPage;
@@ -28,7 +32,7 @@ public class FacebookLoginTest extends BaseTest{
 	public void browser() throws InterruptedException {
 		driver=LaunchBrowser.openBrowser("https://www.facebook.com/");
 	}
-	@Test
+	@Test(priority = 1)
 	public void enterValidCredentials() throws InterruptedException, IOException {
 		extentTest=extentReports.createTest("enterValidCredentials");
 		//WebDriver driver=LaunchBrowser.openBrowser("https://www.facebook.com/");
@@ -40,18 +44,19 @@ public class FacebookLoginTest extends BaseTest{
 		}
 	
 	
-	@Test
+	@Test(priority = 2)
 	public void enterValidUnameAndInvalidPassword() throws InterruptedException, IOException {
 		extentTest=extentReports.createTest("enterValidUnameAndInvalidPassword");
 		//WebDriver driver=LaunchBrowser.openBrowser("https://www.facebook.com/");
 		FacebookLoginPage loginTest=new FacebookLoginPage(driver);
 		loginTest.enterEmailID("Arise");
 		loginTest.enterPassword("123");
+		Thread.sleep(500);
 		loginTest.clickLoginButton();
 		Screenshot.takeScreenshot(driver, "name1");
 	}
 	
-	@Test
+	@Test(priority = 3,dependsOnMethods = "enterValidUnameAndInvalidPassword")
 	public void enetrInvalidUnameAndValidPassword() throws InterruptedException, IOException {
 		extentTest=extentReports.createTest("enetrInvalidUnameAndValidPassword");
 		//WebDriver driver=LaunchBrowser.openBrowser("https://www.facebook.com/");
@@ -62,6 +67,26 @@ public class FacebookLoginTest extends BaseTest{
 		loginTest.clickLoginButton();
 		Screenshot.takeScreenshot(driver, "name2");
 		
+	}
+	@AfterMethod
+	public void closeBrowser(ITestResult result) {
+		if(result.getStatus()==result.SUCCESS)
+		{
+			extentTest.log(Status.PASS, result.getName());
+		}
+		else if(result.getStatus()==result.FAILURE)
+		{
+			extentTest.log(Status.FAIL,result.getName());
+		}
+		else if(result.getStatus()==result.SKIP)
+		{
+			extentTest.log(Status.SKIP,result.getName());
+		}
+		driver.close();
+	}
+	@AfterClass
+	public void flushData() {
+		extentReports.flush();
 	}
 
 }
